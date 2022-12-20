fd029bd7947f292edf2c404dc647f8aa69f2728a
NarberaL <63050180@kmitl.ac.th>
2022-12-20 15:43:11 +0700

    [13]Login

--- app.py
+++ app.py
@@ -83,5 +83,9 @@ def sendBName():
 def register():
     return render_template("register.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__" :
     app.run(debug=True)
